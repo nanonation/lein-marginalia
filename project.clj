8ce18591e94121d5f763f914e81661d56de1c2b3
Joe Smith <JoeSmith@nanonation.net>
2012-05-18 12:30:38 -0500

    version bump

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject net.nanonation/lein-marginalia "0.8.0-SNAPSHOT"
+(defproject net.nanonation/lein-marginalia "0.8.1-SNAPSHOT"
   :description "Leiningen plugin for Marginalia."
   :url "http://github.com/fogus/marginalia"
-  :dependencies [[net.nanonation/marginalia "0.8.0-SNAPSHOT"]]
+  :dependencies [[net.nanonation/marginalia "0.8.1-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
