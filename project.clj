9db5148e92151cc6fb8a57a9d6d6992ad06406a8
Joe Smith <JoeSmith@nanonation.net>
2012-05-18 12:26:52 -0500

    project file update

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject lein-marginalia "0.8.0-SNAPSHOT"
+(defproject net.nanonation/lein-marginalia "0.8.0-SNAPSHOT"
   :description "Leiningen plugin for Marginalia."
   :url "http://github.com/fogus/marginalia"
-  :dependencies [[marginalia "0.8.0-SNAPSHOT"]])
+  :dependencies [[net.nanonation/marginalia "0.8.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
